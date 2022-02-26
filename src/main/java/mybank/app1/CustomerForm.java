c7a39a1d1868f25c69d5629639f02618367da9f6
Waihon Yew <yewwaihon@gmail.com>
2022-02-26 12:55:06 +0800

    Validate customer form

--- src/main/java/mybank/app1/CustomerForm.java
+++ src/main/java/mybank/app1/CustomerForm.java
@@ -1,29 +1,44 @@
 package mybank.app1;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
 import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionMapping;
 
 public class CustomerForm extends ActionForm {
     private String firstName;
     private String lastName;
-    
+
     public CustomerForm() {
         firstName = "";
         lastName = "";
     }
-    
+
     public String getFirstName() {
         return firstName;
     }
-    
+
     public void setFirstName(String s) {
         this.firstName = s;
     }
-    
+
     public String getLastName() {
         return lastName;
     }
-    
+
     public void setLastName(String s) {
         this.lastName = s;
+    }
+
+    @Override
+    public ActionErrors validate(ActionMapping mapping,
+                                 HttpServletRequest request) {
+        ActionErrors errors = new ActionErrors();
+        if (firstName == null || firstName.trim().equals("")) {
+            errors.add("firstName", new ActionError("error.cust.firstname.null"));
+        }
+        return errors;
     }
 }
